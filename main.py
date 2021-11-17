60d6f550f87691f07033ff273b41e07d83d2f61d
Boyko Alexey <elorian@yandex.ru>
2021-11-17 14:14:28 +0300

    add opportunity fo download spacexdata last images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+from urllib.parse import urlparse
 
 
 def image_downloader(source, destination):
@@ -11,16 +12,26 @@ def image_downloader(source, destination):
 
 
 if __name__ == "__main__":
-    filename = './images/hubble.jpeg'
-    url_wiki_img = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    # filename = './images/hubble.jpeg'
+    # url_wiki_img = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
     Path('./images').mkdir(parents=True, exist_ok=True)
     url_spacex = 'https://api.spacexdata.com/v4/launches/latest'
 
-    image_downloader(url_wiki_img, filename)
-
     response = requests.get(url_spacex)
     response.raise_for_status()
-    print(response.json()['links']['patch'])
+    roster_of_links = response.json()['links']['patch']
+    # print(roster_of_links)
+    for link_image in roster_of_links:
+        cleared_link = roster_of_links[link_image]
+        # print(cleared_link)
+        parsed_link = urlparse(cleared_link)
+        image_name_from_link = parsed_link.path
+        filename_to_write = f'./images{image_name_from_link}'
+        image_downloader(cleared_link, filename_to_write)
+        # print(filename_to_write)
+
+
+    # image_downloader(url_wiki_img, filename)
 
 
 
