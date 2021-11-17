078bc16a8c18dfc7399107c7c84d7b9dc4ad2f6c
Boyko Alexey <elorian@yandex.ru>
2021-11-17 15:54:26 +0300

    added extansion split code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import logging
 import requests
+import os
 from pathlib import Path
 from urllib.parse import urlparse
+
+from dotenv import load_dotenv
 
 
 def image_downloader(source, destination):
@@ -28,6 +31,7 @@ def fetch_spacex_last_launch(link_to_download):
 
 
 if __name__ == "__main__":
+    load_dotenv()
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
@@ -36,8 +40,24 @@ if __name__ == "__main__":
     )
     Path('./images').mkdir(parents=True, exist_ok=True)
     url_spacex = 'https://api.spacexdata.com/v4/launches/latest'
+    url_nasa = 'https://api.nasa.gov/planetary/apod'
+    key_nasa = os.getenv('NASA_KEY')
+    params = {'api_key': key_nasa}
+    nasa_response = requests.get(url_nasa, params=params)
+    nasa_response.raise_for_status()
+    # print(nasa_response.json())
+    nasa_image_url = nasa_response.json()['hdurl']
+    print(nasa_image_url)
+    nasa_parse_url = urlparse(nasa_image_url)
+    path_to_file = nasa_parse_url.path
+    print(path_to_file)
+    print(os.path.split(path_to_file))
+    name_of_file = os.path.split(path_to_file)[1]
+    print(name_of_file)
+    print(os.path.splitext(name_of_file)[1])
 
-    fetch_spacex_last_launch(url_spacex)
+
+    #fetch_spacex_last_launch(url_spacex)
 
 
 
