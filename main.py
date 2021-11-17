837312fadf4df42e2a69a830e94ee0c81875cd45
Boyko Alexey <elorian@yandex.ru>
2021-11-17 11:55:50 +0300

    The <image_downloader> func allocated to download images

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import requests
+from pathlib import Path
 
-filename = 'hubble.jpeg'
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
-response = requests.get(url)
-response.raise_for_status()
+def image_downloader(source, destination):
+    response = requests.get(source)
+    response.raise_for_status()
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+    with open(destination, 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == "__main__":
+    filename = './images/hubble.jpeg'
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    Path('./images').mkdir(parents=True, exist_ok=True)
+
+    image_downloader(url, filename)
+
+
+
